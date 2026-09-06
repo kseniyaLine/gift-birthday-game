ea030debcdddb6a3a2e4c5404d94dfbe374243f9
pupko <pupko.k@promail366.com>
2026-09-06 17:04:09 +0300

    speed

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,7 @@ const PLAYER_ANIM_SPEED = 4;
 const OBS_ROWS = 8;
 const OBS_COLS = 9;
 const OBS_TOTAL_FRAMES = 70;
-const OBS_ANIM_SPEED = 2;
+const OBS_ANIM_SPEED = 3;
 const OBS_SIZE = 40;
 const OBS_HITBOX_RADIUS = 16;
 const OBS_SPAWN_RATE = 240; 
@@ -27,7 +27,7 @@ const BASE_WALL = 60;
 const PROTRUSION_WALL = 90;
 const SEGMENT_HEIGHT = 60;
 const JUMP_SPEED = 14;
-const SCROLL_SPEED = 2;
+const SCROLL_SPEED = 3;
 
 const COIN_SIZE = 32;
 const COIN_HITBOX = 14;
